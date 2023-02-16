429c98aa0326e2801d3f3ec8f9858f5cbf8b1ba7
TonySilva7 <tony.deve7oper@gmail.com>
2023-02-16 15:18:18 -0300

    refactor: adjust responsive

--- lib/Pages/auth_page.dart
+++ lib/Pages/auth_page.dart
@@ -22,41 +22,45 @@ class AuthPage extends StatelessWidget {
             ),
           ),
         ),
-        SizedBox(
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(bottom: 20),
-                padding: EdgeInsets.symmetric(
-                  vertical: 6,
-                  horizontal: 60,
-                ),
-                transform: Matrix4.rotationZ(-8 * 3.1415927 / 180)..translate(-8.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Theme.of(context).colorScheme.primary,
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.black26,
-                      blurRadius: 10,
-                      offset: Offset(0, 3),
+        Center(
+          child: SingleChildScrollView(
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 20),
+                    padding: EdgeInsets.symmetric(
+                      vertical: 6,
+                      horizontal: 60,
                     ),
-                  ],
-                ),
-                child: Text(
-                  'Tony\'s Store',
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontFamily: 'Anton',
-                    color: Colors.white,
+                    transform: Matrix4.rotationZ(-8 * 3.1415927 / 180)..translate(-8.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Theme.of(context).colorScheme.primary,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 10,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Text(
+                      'Tony\'s Store',
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontFamily: 'Anton',
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
-                ),
+                  AuthForm(),
+                ],
               ),
-              AuthForm(),
-            ],
+            ),
           ),
         )
       ],
